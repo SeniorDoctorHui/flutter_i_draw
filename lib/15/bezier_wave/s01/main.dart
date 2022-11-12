0578636099fa8623895327259193fbf3673836d8
huige <nalanrumeng1113@163.com>
2022-11-12 16:56:22 +0800

    开发分支3560功能,补充注释

--- lib/15/bezier_wave/s01/main.dart
+++ lib/15/bezier_wave/s01/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_i_draw/15/bezier_wave/s01/paper.dart';
 
+//增加注释
 void main() {
   // 确定初始化
   WidgetsFlutterBinding.ensureInitialized();
