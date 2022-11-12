7657d2c69b529aeadade98a4fe5201d3cc43c2ae
huige <nalanrumeng1113@163.com>
2022-11-12 17:04:22 +0800

    3560修改main.dart注释1

--- lib/15/bezier_wave/s01/main.dart
+++ lib/15/bezier_wave/s01/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_i_draw/15/bezier_wave/s01/paper.dart';
 
-//增加注释345
+//增加注释345135
 void main() {
   // 确定初始化
   WidgetsFlutterBinding.ensureInitialized();
